ecae7cc74dafe84229cd1212dc6600160144cd98
David Garcia <garciamorales111.o.o.f@gmail.com>
2022-04-04 20:59:07 -0600

    feature/add_fonts

--- src/components/atoms/Title/style.ts
+++ src/components/atoms/Title/style.ts
@@ -1,8 +1,10 @@
 import {StyleSheet} from 'react-native';
+import {fonts} from '../../../utils/fonts';
 import {typography} from '../../../utils/typography';
 
 export const styles = StyleSheet.create({
   title: {
     fontSize: typography.h1,
+    fontFamily: fonts.bold,
   },
 });
